460fbba9776a9a7d71f77f4e00cd94882af1b00a
RhysyT <61121117+RhysyT@users.noreply.github.com>
2025-08-21 10:11:54 +0200

    Update GetImages3.py

--- GetImages3.py
+++ GetImages3.py
@@ -202,13 +202,13 @@ with c6:
 with c7:
     name_tag = st.text_input("Name resolver / download file name", "Target", help="Optional, but useful for specifying the name of the file ahead of time for downloads")
 with c8:
-    resolve = st.button("Resolve coordinates")
+    resolve = st.button("Resolve coordinates", help='Attempts to find the coordinates of the named object (equatorial only, J2000)')
 
 # Experimental name resolver
 if resolve == True:
     try:
         coords = SkyCoord.from_name(name_tag)
-        st.write(coords)
+        st.write(coords.ra, coords.dec)
     except:
         pass
 
