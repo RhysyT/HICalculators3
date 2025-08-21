4432cf8d3bf33dcca84a52124dd358e3d50976f6
RhysyT <61121117+RhysyT@users.noreply.github.com>
2025-08-21 11:48:06 +0200

    Update GetImages3.py

--- GetImages3.py
+++ GetImages3.py
@@ -196,7 +196,7 @@ c1, c2, c3, c4 = st.columns([1, 1, 1, 1])    # ChatGPT preferred 1.2, 1.2 for th
 with c1:
     coord_ra = st.text_input("RA  —  decimal degrees or HH:MM:SS", value=st.session_state['coord_ra_val'], help="Must be J2000, but fairly liberal. Enter something sensible and astropy will try its best")
 with c2:
-    coord_dec = st.text_input("Dec  —  decimal degrees or DD:MM:SS", value=st.session_state['coord_ra_dec'], help="Must be J2000, but fairly liberal. Enter something sensible and astropy will try its best")
+    coord_dec = st.text_input("Dec  —  decimal degrees or DD:MM:SS", value=st.session_state['coord_ra_val'], help="Must be J2000, but fairly liberal. Enter something sensible and astropy will try its best")
 with c3:
     fov_value = st.number_input("Field of view value", min_value=0.001, value=3.5, step=0.5, format="%.3f", help="Assumes a simple square field of view")
 with c4:
