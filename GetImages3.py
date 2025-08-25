7c99e5daecf9486baf8f7ba6e21dbf65eb195e4e
RhysyT <61121117+RhysyT@users.noreply.github.com>
2025-08-25 12:28:14 +0200

    Update GetImages3.py

--- GetImages3.py
+++ GetImages3.py
@@ -338,7 +338,8 @@ with format_col:
 
 
 # Row 4 - print the selected data resolution
-hips_id = str(SURVEYS[survey_name])
+# Simpler survey name dictionary
+st.write(SURVEYS[survey_name]))
 st.write("SDSS g:", hips_scale_arcsec_per_pix(hips_id), "arcsec/pixel")
 st.write("GALEX NUV:", hips_scale_arcsec_per_pix("CDS/P/GALEXGR6/AIS/NUV"), "arcsec/pixel")
 st.write("2MASS J:", hips_scale_arcsec_per_pix("CDS/P/2MASS/J"), "arcsec/pixel")
