fce40f17dfbff85ff3bf9126c2d6be095252ba30
RhysyT <61121117+RhysyT@users.noreply.github.com>
2025-08-27 14:22:17 +0200

    Update UITesting.py

--- UITesting.py
+++ UITesting.py
@@ -19,9 +19,10 @@ st.set_page_config(page_title="Minimal number_input test", layout="wide")
 #    a = st.number_input("Sidebar number", value=0.0, step=1.0, format="%.3f", key="sidebar_num")
 
 st.header("Main area")
-b = st.number_input("Main number", value=0.0, step=0.01, format="%.3f", key="main_num")
+b = st.number_input("Main number", value=0.0)
 
 st.write("You typed:", b)
+
 
 
 
