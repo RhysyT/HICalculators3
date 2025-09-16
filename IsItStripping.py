c8e097634204dc25edfd15adcbbb7317599dbfff
RhysyT <61121117+RhysyT@users.noreply.github.com>
2025-09-16 11:27:31 +0200

    Update IsItStripping.py

--- IsItStripping.py
+++ IsItStripping.py
@@ -297,11 +297,11 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Required pressure from H I deficiency (eq. 27)")
-    st.markdown(f"p_def [x1000 cm⁻³ (km/s)²] :")
+    st.markdown(f"p$_{def}$ [1000 cm⁻³ (km/s)²] :")
     #st.markdown(f"## {pdef:,.3f}")
     #st.caption(f"= {pdef/1000.0:.3f}  × 1000 cm⁻³ (km/s)²") 
-    st.markdown(f"## {pdef/1000.0:.3f}  × 1000 cm⁻³ (km/s)²")
-    st.caption(f" = {pdef:,.3f}")
+    st.markdown(f"## {pdef/1000.0:.3f} ")
+    st.caption(f" = {pdef:,.3f} [cm⁻³ (km/s)²]")
 
     
     
